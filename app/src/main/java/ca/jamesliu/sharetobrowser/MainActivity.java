e270aa77a550fe66f58e813de0304d6661ddd05b
James Liu <t-jaliu@microsoft.com>
2016-07-11 07:12:31 -0700

    this pretty much works

--- app/src/main/java/ca/jamesliu/sharetobrowser/MainActivity.java
+++ app/src/main/java/ca/jamesliu/sharetobrowser/MainActivity.java
@@ -1,7 +1,10 @@
 package ca.jamesliu.sharetobrowser;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent launchIntent = getIntent();
+        String action = launchIntent.getAction();
+        String type = launchIntent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                String sharedText = launchIntent.getStringExtra(Intent.EXTRA_TEXT);
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(sharedText));
+                this.startActivity(i);
+            } else {
+                Toast.makeText(this, "Data not supported.", Toast.LENGTH_SHORT);
+            }
+        }
     }
+
 }
